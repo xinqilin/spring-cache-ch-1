61ef25f8b6421fbebfbec102620ceef2ae171db3
BillLin1209 <zzx123bill@gmail.com>
2020-05-30 23:21:38 +0800

    config properties

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# spring-cache-ch-1
+# spring-cache-ch-1 練習
+
+>1. DB<br>
+2. POJO<br>
+3. 配置持久層<br>
+   1.connection DB<br>
+   2.使用myBatis mapper
+   
+   
---
+++ src/main/java/com/bill/cache/bean/Department.java
@@ -0,0 +1,48 @@
+package com.bill.cache.bean;
+
+public class Department {
+	private Integer id;
+	private String departmentName;
+	
+	
+	public Department() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Department(Integer id, String departmentName) {
+		super();
+		this.id = id;
+		this.departmentName = departmentName;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", departmentName=" + departmentName + "]";
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+	
+	
+}
