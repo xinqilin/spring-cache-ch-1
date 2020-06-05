4b474f19f5373d61c2c39f5483b52aaef436bba2
BillLin1209 <zzx123bill@gmail.com>
2020-06-05 22:57:12 +0800

    add dept service

--- src/main/java/com/bill/cache/controller/DepartmentController.java
+++ src/main/java/com/bill/cache/controller/DepartmentController.java
@@ -1,5 +1,39 @@
 package com.bill.cache.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.bill.cache.bean.Department;
+import com.bill.cache.service.DepartmentService;
+
+@Controller
 public class DepartmentController {
 
+	
+	@Autowired
+	DepartmentService deptService;
+	
+	
+	@GetMapping("/dept/{id}")
+	public Department getDeptById(@PathVariable("id") Integer id) {
+		return deptService.getDeptById(id);
+	}
+	
+	@GetMapping("/dept")
+	public Department updateDept(Department dept) {
+		return deptService.updateDept(dept);
+	}
+	
+	@GetMapping("/delDept/{id}")
+	public String deleteEmp(@PathVariable("id") Integer id) {
+		deptService.deleteDept(id);
+		return "Delete Success!!!";
+	}
+	
+	@GetMapping("/deptName/{name}")
+	public Department getDeptByName(@PathVariable("name") String name) {
+		return deptService.getDeptByName(name);
+	}
 }
