8293ac73c27f0fb61143693dc72abbf013120f0b
BillLin1209 <zzx123bill@gmail.com>
2020-06-06 17:21:02 +0800

    dept service setting done!

--- src/main/java/com/bill/cache/service/DepartmentService.java
+++ src/main/java/com/bill/cache/service/DepartmentService.java
@@ -1,6 +1,10 @@
 package com.bill.cache.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import com.bill.cache.bean.Department;
@@ -12,21 +16,32 @@ public class DepartmentService {
 	@Autowired
 	DepartmentMapper deptMapper;
 	
+	@Cacheable(cacheNames= {"dept"})
 	public Department getDeptById(Integer id) {
 		return deptMapper.getDeptById(id);
 	}
 	
+	
+	@CachePut(cacheNames= "dept" , key="#result.id")
 	public Department updateDept(Department dept) {
 		deptMapper.updateDept(dept);
 		return getDeptById(dept.getId());
 	}
 	
+	
+	@CacheEvict(cacheNames= "dept" , key="#id" ,allEntries=true)
 	public void deleteDept(Integer id) {
 		System.out.println("要delete的deptId: "+id);
 //		假裝有刪  ↓↓↓
 //		deptMapper.deleteDept(id);
 	}
 	
+	
+	@Caching(
+			cacheable= {
+					@Cacheable(cacheNames="dept",key="#name")
+			}
+		)
 	public Department getDeptByName(String name) {
 		return deptMapper.getDeptByName(name);
 	}
